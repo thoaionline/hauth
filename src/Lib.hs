0c9598ad7914f1f97296ef4086651b60c6900c6b
Thoai Nguyen <me@thoaionline.com>
2020-12-25 22:42:19 +1100

    Add sample json

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,7 @@ module Lib
   ( someFunc,
     timePlay,
     textPlay,
+    jsonPlay,
   )
 where
 
@@ -10,6 +11,7 @@ import Data.Time.Clock.POSIX
 import Data.Time.Lens
 import Data.Time.LocalTime (getZonedTime)
 import Text.Regex.PCRE.Heavy
+import NeatInterpolation
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -38,3 +40,19 @@ textPlay = do
   print $ match "heyabc" reg1
   where
     match str reg = concat [str, " ", if str =~ reg then asText "matches" else asText "doesn't match", " the specified regex"] :: Text
+
+jsonPlay :: IO ()
+jsonPlay = do
+  let sample = [text|
+    {
+      "a": "b",
+      "c": {
+        "d": 5,
+        "e: {
+          "f": 7.0,
+          "g": null
+        }
+      }
+    }
+  |]
+  print sample
